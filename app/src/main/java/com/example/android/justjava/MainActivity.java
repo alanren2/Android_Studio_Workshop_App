b53d52c94fcceee2ef2281c36f2c8036ac72480f
Alan Ren <alanren2@illinois.edu>
2016-10-31 19:11:51 -0500

    cleaned up code.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -8,9 +8,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import java.text.NumberFormat;
 
-/**
- * This app displays an order form to order coffee.
- */
 public class MainActivity extends AppCompatActivity {
 
     double quantity = 0;
@@ -22,9 +19,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * This method is called when the order button is clicked.
-     */
     public void submitOrder(View view) {
         EditText amount_text = (EditText) findViewById(R.id.editText);
         tax = Double.parseDouble(amount_text.getText().toString());
@@ -32,30 +26,22 @@ public class MainActivity extends AppCompatActivity {
         displayPrice(quantity1);
     }
 
-    /**
-     * This method displays the given quantity value on the screen.
-     */
     private void display(double number) {
         TextView quantityTextView = (TextView) findViewById(R.id.number_text_view);
         quantityTextView.setText("" + (int)number);
 
     }
 
-    /**
-     * This method displays the given price on the screen.
-     */
     private void displayPrice(double number) {
         TextView priceTextView = (TextView) findViewById(R.id.priceNumber_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
-    // This is when you press the add button.
     public void increment(View view)   {
         quantity++;
         display(quantity);
     }
 
-    // This is when you press the subtract button.
     public void decrement(View view)   {
         quantity--;
         display(quantity);
